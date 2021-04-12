a0ef1a597ab9d663e8a9633bcd44318c6831ac92
Eoin O'Cleirigh <Eoin@iEoin.com>
2021-04-12 22:41:49 +1200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Eoin O'Cleirigh on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
